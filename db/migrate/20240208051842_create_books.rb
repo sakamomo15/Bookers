7d1e9e3983df9bb4c9b70020bfa1bf16567f5dd9
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-10 03:24:05 +0000

    3-課題2-new

--- db/migrate/20240208051842_create_books.rb
+++ db/migrate/20240208051842_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
-    create_table :books do |t|
+    create_table :books do |t| #model:Book, table:books, file:book.rb
       t.string :title
       t.string :body
       t.timestamps

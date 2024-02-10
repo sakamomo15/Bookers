647dcad7914ec8e79ed5b5eabf6c5d9a1410c8cb
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-10 18:46:50 +0000

    3-課題2-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+    # indexに移動
   end
 
   def create
     book = Book.new(book_params) #viewはないのでローカル変数
     book.save
-    redirect_to '/books/show'
+    redirect_to book_path(book.id) #idに対応したshowに遷移するため引数必須
   end
 
   def index
     @books = Book.all
+    @book = Book.new # newから移動
+    # @book_show = Book.find(params[:id]) # showからコピー
+
   end
 
   def show
@@ -20,11 +23,17 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
     # book => createのローカル変数
   end
-  
+
 end
